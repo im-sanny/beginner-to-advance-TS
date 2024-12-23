0caec45655b70d104291ec20b58ed454acf2c58d
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2024-12-23 23:58:45 +0600

    add: added some core features of ts

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
 let message: string = 'Hello TS';
 console.log(message);
+
+let firstName: string = 'Rasel';
+let age: number = 24;
+let isEmployed: boolean = false;
+
+let fruits: string[] = ['mango', 'litchi'];
+let numbers: number[] = [1, 2, 3, 4];
+
+let user: [string, number, boolean];
+user = ['Rasel', 24, true];
+
+
