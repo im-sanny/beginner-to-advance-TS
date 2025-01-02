84abf0164b814b66580bb788798711755b101d58
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-01-03 02:15:16 +0600

    add: ts modules created and added in the modules file

---
+++ modules/app.ts
@@ -0,0 +1,4 @@
+import { add, PI } from './math';
+
+console.log(add(5, 7));
+console.log(PI);
---
+++ modules/math.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PI = void 0;
+exports.add = add;
+function add(a, b) {
+    return a + b;
+}
+exports.PI = 3.14;
